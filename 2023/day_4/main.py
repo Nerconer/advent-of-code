66e5f6d02f2e95f4891fb039c2c474366a85806d
David Sola Sole <david.sola.sole@gmail.com>
2023-12-04 19:45:07 +0100

    feat: day 4 2023 🎂

---
+++ 2023/day_4/main.py
@@ -0,0 +1,32 @@
+file = open(0).read()
+lines = file.strip().splitlines()
+
+# Part 1
+total_points = 0
+# Part 2
+card_list = [1] * len(lines)
+
+for index, full_line in enumerate(lines):
+    card_matches = 0
+    winning_numbers, user_numbers = [k.split() for k in full_line.split(':')[1].strip().split(' | ')]
+    
+    for number in user_numbers:
+        if number in winning_numbers:
+            card_matches += 1
+    
+    for card_match in range(card_list[index]):
+        for ii in range(card_matches):
+            card_list[index+ ii + 1] +=1
+
+    if card_matches == 0:
+        continue
+    total_points += 2 ** (card_matches - 1)
+     
+total_scratch_cards = sum(card_list)
+    
+print(f'Part 1: {total_points}')
+print(f'Part 2: {total_scratch_cards}')
+
+# Aliases
+# aos to check againt full input.txt file
+# aot to check againt test.txt file
