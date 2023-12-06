0722fe5909b06c56dbc5e715c4f459525e1f7d2d
David Sola Sole <david.sola.sole@gmail.com>
2023-12-06 04:42:11 +0100

    feat: 2022 day 5

---
+++ 2022/day_5/main.py
@@ -0,0 +1,59 @@
+input = open(0).read().split('\n\n')
+
+number_of_stacks = int(input[0].split('\n')[-1].split()[-1])
+
+stacks = [ [] for _ in range(number_of_stacks) ]
+    
+for row in input[0].splitlines()[0:-1]:
+    stack = []
+    for index, elem in enumerate(range(0, len(row), 4)):
+        if row[elem:elem+4] != '    ' and row[elem:elem+4] != '   ':
+            stacks[index].append(row[elem:elem+4].replace('[', '').replace(']', ''))
+    
+for stack in stacks:
+    stack.reverse()
+    
+stacks_part_two = [x[:] for x in stacks]
+    
+commands = input[1].splitlines()
+
+def part_one() -> str:
+    for command in commands:
+        m = int(command.split()[1])
+        f = int(command.split()[3])
+        t = int(command.split()[5])
+        
+        while(m):
+            val = stacks[f-1].pop().strip()
+            stacks[t-1].append(val)
+            m -= 1
+
+    result = ""
+    for stack in stacks:
+        result += stack.pop()
+        
+    return result
+
+def part_two() -> str:
+    for command in commands:
+        m = int(command.split()[1])
+        f = int(command.split()[3])
+        t = int(command.split()[5])
+        
+        arr = []
+        while(m):
+            elem = stacks_part_two[f-1].pop().strip()
+            arr.append(elem)
+            m -= 1
+        arr.reverse()
+        for elem in arr:
+            stacks_part_two[t-1].append(elem)
+
+    result = ""
+    for stack in stacks_part_two:
+        result += stack.pop()
+        
+    return result
+
+print(f'Part 1: {part_one()}')
+print(f'Part 2: {part_two()}')
