d4573ee3bc68b19430b930163de4dd6b490bf995
David Sola Sole <david.sola.sole@gmail.com>
2023-12-06 14:48:05 +0100

    feat: 2023 day 6 binary search optimization

--- 2023/day_6/main.py
+++ 2023/day_6/main.py
@@ -47,7 +47,36 @@ def part_two():
     total_combinations *= 2
     
     return total_combinations + 1
+
+def part_two_binary_search():
+    times = input[0].split(':')[1]
+    distances = (input[1].split(':')[1])
     
+    times = times.split()
+    distances = distances.split()
+    
+    total_time = int(''.join(times))
+    total_distance = int(''.join(distances))
+        
+    def binary_search(low, high, min_index, total_distance):
+        if high > low:
+            mid = (high + low) // 2
+            distance_traveled = mid * (total_time - mid)
+            if distance_traveled >= total_distance:
+                min_index = mid
+                # Go left
+                return binary_search(low, mid, min_index, total_distance)
+            # Go right
+            else:
+                return binary_search(mid + 1, high, mid, total_distance)
+        else:
+            return min_index
+
+    first_valid = binary_search(0, total_time // 2, total_time // 2, total_distance)
+    total_combination = ((total_time // 2 - first_valid) * 2) + 1
+    return total_combination
+
+
 part_one_start = datetime.datetime.now()
 print(f'Part 1: {part_one()}')
 part_one_end = datetime.datetime.now()
@@ -55,5 +84,9 @@ print(f'Part 1 execution time: {(part_one_end - part_one_start).total_seconds() 
 
 print(f'Part 2: {part_two()}')
 part_two_end = datetime.datetime.now()
-print(f'Part 2 execution time: {(part_two_end - part_one_end).total_seconds() * 1000} ms')
+print(f'Part 2 execution time: {(part_two_end - part_one_end).total_seconds() * 1000} ms\n')
+
+print(f'Part 2 binary search: {part_two_binary_search()}')
+part_two_binary_search_end = datetime.datetime.now()
+print(f'Part 2 binary search execution time: {(part_two_binary_search_end - part_two_end).total_seconds() * 1000} ms')
         
