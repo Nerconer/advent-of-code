af47d845e7237d0c21c1de46b5f5c8612a1e3778
David Sola Sole <david.sola.sole@gmail.com>
2023-12-10 21:30:40 +0100

    feat: 2023 day 10 part 2

--- 2023/day_10/main.py
+++ 2023/day_10/main.py
@@ -7,94 +7,53 @@ matrix = []
 for line in grid:
     matrix.append(list(line))
     
-def find_start_point(matrix: list[list[str]]) -> tuple[int, int, int]:
+def find_start_point(matrix: list[list[str]]) -> tuple[int, int]:
     for i in range(len(matrix)):
         for j in range(len(matrix[i])):
             if matrix[i][j] == 'S':
-                return i, j, 0
+                return i, j
     else:
         raise ValueError('No starting point found')
 
-def find_longest_path(matrix: list[list[str]], start_point: tuple[int, int, int]):
-    def bfs(matrix: list[list[str]], start_point: tuple[int, int, int]):
-        valid_up_pipes = ['F', '|', '7']
-        valid_down_pipes = ['L', '|', 'J']
-        valid_right_pipes = ['J', '-', '7']
-        valid_left_pipes = ['L', '-', 'F']
+def find_longest_path(matrix: list[list[str]], start_point: tuple[int, int]): 
+    valid_up_pipes = 'F|7'
+    valid_down_pipes = 'L|J'
+    valid_right_pipes = 'J-7'
+    valid_left_pipes = 'L-F'
+    
+    visited = {start_point}
+    queue = deque([start_point])
+            
+    while(queue):
+        i, j = queue.popleft()
+        ch = matrix[i][j]
         
+        visited.add((i, j))
         
-        visited = set([(start_point[0], start_point[1])])
-        queue = deque([start_point])
+        # Check up
+        if i > 0 and ch in "S|JL" and matrix[i - 1][j] in valid_up_pipes and (i - 1, j) not in visited:
+            queue.append((i - 1, j))
         
-        max_depth = 0
+        # Check down
+        if i < len(matrix) - 1 and ch in "S|7F" and matrix[i + 1][j] in valid_down_pipes and (i + 1, j) not in visited:
+            queue.append((i + 1, j))
         
-        while(queue):
-            curr_p = queue.popleft()
-            depth = curr_p[2]
-                        
-            #visited.add((current_point[0], current_point[1]))
-            
-            if matrix[curr_p[0]][curr_p[1]] == 'S' and depth > 2:
-                print(f'Found S at {curr_p} with depth {depth}')
-                
-            
-            left_position = (curr_p[0], curr_p[1] - 1)
-            right_position = (curr_p[0], curr_p[1] + 1)
-            up_position = (curr_p[0] - 1, curr_p[1])
-            down_position = (curr_p[0] + 1, curr_p[1])
-            
-            # Check left
-            if curr_p[1] - 1 >= 0 and matrix[curr_p[0]][curr_p[1] - 1] in valid_left_pipes and left_position not in visited:
-                queue.append(left_position + (depth + 1,))
-                max_depth = max(max_depth, depth + 1)
-                visited.add(left_position)
-             
-            # Check right
-            if curr_p[1] + 1 < len(matrix[curr_p[0]]) and matrix[curr_p[0]][curr_p[1] + 1] in valid_right_pipes and right_position not in visited:
-                queue.append(right_position + (depth + 1,))
-                max_depth = max(max_depth, depth + 1)
-                visited.add(right_position)
-                
-            # Check up
-            if curr_p[0] - 1 >= 0 and matrix[curr_p[0] - 1][curr_p[1]] in valid_up_pipes and up_position not in visited:
-                queue.append(up_position + (depth + 1,))
-                max_depth = max(max_depth, depth + 1)
-                visited.add(up_position)
-            
-            # Check down
-            if curr_p[0] + 1 < len(matrix) and matrix[curr_p[0] + 1][curr_p[1]] in valid_down_pipes and down_position not in visited:
-                queue.append(down_position + (depth + 1,))
-                max_depth = max(max_depth, depth + 1)
-                visited.add(down_position)
-                
-        return max_depth, visited
-            
-  
-    depth, visited = bfs(matrix, start_point)
-    return depth, visited
+        # Check left
+        if j > 0 and ch in "S-J7" and matrix[i][j - 1] in valid_left_pipes and (i, j - 1) not in visited:
+            queue.append((i, j - 1))
+        
+        # Check right
+        if j < len(matrix[i]) - 1 and ch in "S-LF" and matrix[i][j + 1] in valid_right_pipes and (i, j + 1) not in visited:
+            queue.append((i, j + 1))
+         
+    return visited
         
     
-def part_one(matrix: list[list[str]], starting_point: tuple[int, int, int]) -> int:
-    return find_longest_path(matrix, starting_point)[0]
+def part_one(matrix: list[list[str]], starting_point: tuple[int, int]) -> int:
+    return len(find_longest_path(matrix, starting_point)) // 2
 
-def part_two(grid, starting_point: tuple[int, int, int]):
-    grid = [row.replace("S", "|") for row in grid]
-    
-    *_, visited = find_longest_path(matrix, starting_point)
-    
-    # Clean map replacing junk pipes with dots
-    for i in range(len(matrix)):
-            for j in range(len(matrix[i])):
-                if (i, j) not in visited:
-                    matrix[i][j] = '.'
-                        
-    for row in grid:
-        within = False
-        riding = False
-        for ch in row:
-            pass
-    """   
-    *_, visited = find_longest_path(matrix, starting_point)
+def part_two(matrix: list[list[str]], starting_point: tuple[int, int]):
+    visited = find_longest_path(matrix, starting_point)
         
     # Clean map replacing junk pipes with dots
     for i in range(len(matrix)):
@@ -102,26 +61,13 @@ def part_two(grid, starting_point: tuple[int, int, int]):
                 if (i, j) not in visited:
                     matrix[i][j] = '.'
     
-    
-    for i, row in enumerate(matrix):
-        s = ''
-        for j, character in enumerate(row):
-            pos = (i, j)
-            if pos in visited:
-                s+=f'\033[1m{character}\033[0m'
-            else:
-                s+=character
-        print(s)    
-    
+
     # Even/Odd check
     inside_tiles = 0
     for i, row in enumerate(matrix):
         for j, character in enumerate(row):
             if character == '.':
-                temp_count = 0
-                # Cast a ray to the right
-                #for k in range(j + 1, len(row)):
-                    
+                # Cast a ray to the left                    
                 xx, c = j - 1, 0
                 while xx >= 0:
                     if (
@@ -132,14 +78,11 @@ def part_two(grid, starting_point: tuple[int, int, int]):
                     ):
                         c += 1
                     xx -= 1
-                if c % 2 == 1:
-                    #print(f'Inside: {i}, {j}')
-                    inside_tiles += 1
-                #inside_tiles += 1 if temp_count % 2 == 1 else 0
+                inside_tiles += 1 if c % 2 == 1 else 0
     return inside_tiles
-    """
+    
 
 starting_point = find_start_point(matrix)
 
 print(f'Part 1: {part_one(matrix, starting_point)}')
-print(f'Part 2: {part_two(grid, starting_point)}')
+print(f'Part 2: {part_two(matrix, starting_point)}')
